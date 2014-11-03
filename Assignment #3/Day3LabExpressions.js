2039733698f68e2ccf29bc5bcf6a141b68ed9efe
arbochris <bocaraca@christians-mbp.local.tld>
2014-11-03 15:45:58 -0500

    Instructional Expressions
    Created a file, and started with Slice of Pie part 1.

---
+++ Assignment #3/Day3LabExpressions.js
@@ -0,0 +1,16 @@
+/**
+ * Created by christian b. arboleda on 11/3/14.
+ Web Programing Fundamentlas, section 1.
+Instructional Expressionals
+ */
+
+//Slice of Pie 1
+
+    //declared and defined the variables
+
+var slicesofpie = 10
+var numberofpeople = 60
+var boxesofpizza = 18
+
+//
+
