93c73af1ca28a85031993ee4949f24c7147a7656
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-03 18:31:30 -0500

    Finished Array File
    We now know everything about oranges.

--- Day 4/js/classNotesArrays.js
+++ Day 4/js/classNotesArrays.js
@@ -5,7 +5,7 @@ WPF 1411
 11-3-2014
  */
 
-//alert("testing to see if it linked");
+alert("drop it like its hot");
 
 
 //start basic array
@@ -32,4 +32,44 @@ console.log(avengerNames);
 console.log(avengerNames.length);
 
 //add another avenger
-avengerNames[avengerNames.length] = "captain america"
+avengerNames[avengerNames.length] = "captain america";
+
+
+//add the black widow to the mix
+avengerNames[avengerNames.length] = "black widow";
+
+//access the last avenger in the array
+console.log(avengerNames[avengerNames.length - 1]);
+
+
+//new method specific to arrays called push
+//it adds whatever we put in automatically to end of array
+avengerNames.push("hawkeye");
+console.log(avengerNames);
+
+// .pop() - method of an array that removes the last item in the array
+//you can save the popped variable (if you want to)
+// set a variable equal to the pop method
+avengerNames.pop();
+console.log(avengerNames);
+
+var soloAvenger = avengerNames.pop();
+console.log(avengerNames);
+console.log(soloAvenger);
+
+
+//pick oranges everyday
+//track how many oranges are picked every day
+var orangeBins=[234, 473, 20, 709];
+
+//how many oranges were picked in total
+//store that in a variable
+var orangeTotal = orangeBins[0] + orangeBins[1] + orangeBins[2] +orangeBins[3];
+console.log(orangeTotal);
+
+console.log(orangeTotal+" is the total number of oranges picked");
+
+//what is the average oranges picked?
+var orangeAverage = orangeTotal/orangeBins.length;
+console.log("the average amount of oranges picked in "+orangeBins.length+" days in "+orangeAverage+".");
+
