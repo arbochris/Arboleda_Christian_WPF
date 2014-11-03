d5e44a0f62e01b297d017864fdc70bbeb85ccad3
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-03 18:51:10 -0500

    Calculated Age Based On Year

---
+++ Day 4/js/Day 4 Prompts.js
@@ -0,0 +1,21 @@
+/*
+Christian Arboleda
+Day 4 Prompts
+WPF - 1411
+11-03-2014
+ */
+
+alert("twerk!!!");
+
+
+//ask user for input
+//prompt() - ask the user to type something in
+var yearBorn = prompt("enter your year of birth:");
+console.log(yearBorn);
+
+//figure out how old we are
+var age = 2014 - yearBorn;
+console.log("you are "+age+" years old.");
+
+
+
