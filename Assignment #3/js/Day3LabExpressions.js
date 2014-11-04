96eb4271a258f3cf534c443159017bc3cfa34a77
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-03 22:56:28 -0500

    Update Part 1: Slice of Pie
    defined variables for given variable nicknames, calculated slices eaten
    and prompted an alert.

--- Assignment #3/js/Day3LabExpressions.js
+++ Assignment #3/js/Day3LabExpressions.js
@@ -7,3 +7,20 @@ Christian Arboleda
 
 //alert("ding, dong");
 
+
+//Part 1: Slice of Pie
+//Calculate the amount of pizza each person will get.
+//the givens:
+var slicesPizza = 10;
+var boxesPizza = 18;
+var numberPeople = 60;
+var slicesEaten = 3;
+
+//now that the givens have been defined I calculate the slices eaten
+
+var slicesEaten = slicesPizza * boxesPizza / numberPeople;
+console.log(slicesEaten);
+alert("Each person ate "+slicesEaten+" slices of pizza at the party.");
+
+//
+
