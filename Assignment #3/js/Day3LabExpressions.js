2b6ae5137992c7b433661af817aac21440e9a57f
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-06 00:38:24 -0500

    Assignment 3 Update
    Finished calculations on discount of item.

--- Assignment #3/js/Day3LabExpressions.js
+++ Assignment #3/js/Day3LabExpressions.js
@@ -36,4 +36,31 @@ var weekBill=5;
 var avgWeekly=120;
 var totalAmnt=weekBill*avgWeekly;
 console.log("you have spent a total of "+totalAmnt+" on groceries this week. That is an average of "+avgWeekly+" per week.");
- 
+
+//Discounts
+
+/*
+ Calculate  the  discounted  price  for  an  item.  Create  an  expression  that  will  calculate  the
+ discounted  price
+ with  and
+ without
+ sales  tax.  (It  is  acceptable  for  the  result  to  have  more  than
+ two  digits  after  the  decimal.  $345.896  for  example.)
+ */
+
+//givens - original price, discount percentage, description of item, sales tax percentage.
+
+var originalPrice=56.99
+var discPerc= 0.07
+var item="food total"
+
+var salesTax=originalPrice*discPerc
+
+console.log(salesTax);
+
+//calculating with and without tax
+var wTax=60.97
+var woTax=56.99
+
+console.log("Your "+item+" was originally "+originalPrice+", but after a "+discPerc+"%, it is now "+woTax+" without tax, and "+wTax+" with tax.");
+
