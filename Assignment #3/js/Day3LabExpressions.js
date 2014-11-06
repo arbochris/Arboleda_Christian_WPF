2ed47bfeb6e65342344786be92e84ddeccc153d2
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-05 23:31:55 -0500

    Update Assignment 3
    Calculated the budget for 5 weeks

--- Assignment #3/js/Day3LabExpressions.js
+++ Assignment #3/js/Day3LabExpressions.js
@@ -19,17 +19,21 @@ var slicesEaten = 3;
 //now that the givens have been defined I calculate the slices eaten
 var slicesEaten = slicesPizza * boxesPizza / numberPeople;
 console.log(slicesEaten); //output
-alert("Each person ate "+slicesEaten+" slices of pizza at the party."); //alert output
+//alert("Each person ate "+slicesEaten+" slices of pizza at the party."); //alert output
 
 
 //Part 2: Slices of Pie
 //Calculate how many slices does Sparky get?
 var sparkyEats = slicesPizza%slicesEaten //calculate the remainder
 console.log(sparkyEats); //output console
-alert("Sparky got "+sparkyEats+" slice(s) of pizza."); //output prompt
+//alert("Sparky got "+sparkyEats+" slice(s) of pizza."); //output prompt
 
 
 //Average Shopping Bill
 //calculate amount spent on groceries.
 
-var
+var weekBill=5;
+var avgWeekly=120;
+var totalAmnt=weekBill*avgWeekly;
+console.log("you have spent a total of "+totalAmnt+" on groceries this week. That is an average of "+avgWeekly+" per week.");
+ 
