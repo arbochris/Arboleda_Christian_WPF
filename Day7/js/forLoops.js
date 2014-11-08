1804ba3fd1919b553f08ab7d16c4fd477f271266
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-07 20:41:09 -0500

    Update on for loops
    class notes finished for the day

--- Day7/js/forLoops.js
+++ Day7/js/forLoops.js
@@ -3,4 +3,27 @@
  */
 
 
-alert("Hi");
+//alert("Hi");
+
+
+//basic for loops
+//initialization of counter; condition to test; incremement of change.
+// i - iteration.
+
+for(var i=0; i<20 ; i++){
+    console.log("i= "+i);
+}
+
+//if doing a reverse count and to get the "0" to show add a "<=" to it
+
+
+//breaks
+
+//create a for loop but if counter variable is 3 stop the loop
+
+for(var j=0; j<5; j++){
+    console.log("j= "+j);
+    if(j===3){
+        break; //this stops your loop from cycling
+    }
+}
