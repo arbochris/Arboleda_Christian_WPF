54c60ffdb4d146ad3df64b3a483bc3e0805d49f0
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-12 20:35:24 -0500

    End of lecture notes
    finished the class notes for the day.

--- Day8/js/returns.js
+++ Day8/js/returns.js
@@ -9,4 +9,49 @@
 //alert("good to go");
 
 
-//retunrning values from a function
+//returning values from a function
+// or how do we get stuff out of "vegas"
+
+//create a function are of rectangle
+
+function calcArea(wid,len){
+    var area = wid*len;
+
+    console.log("the area of the rectangle is "+area);
+
+    //return the area variable to the main code
+    return area;
+
+
+}
+
+
+//function call
+//in front of the function call catch the returned value with a variable
+var returnedArea =calcArea(30,20);
+
+
+console.log(returnedArea);
+
+
+//prompt the user
+
+var width=prompt("please enter a width");
+var length=prompt("please enter a length");
+
+var userRecArea=calcArea(width, length);
+
+var userRecPeri=periRect(width, length);
+
+console.log("the area of the rectangle with a "+width+" and a length of "+length+" is "+userRecArea+ ". \nThe perimeter is "+userRecPeri);
+
+
+//create a function to find perimeter of a rectangle
+
+function periRect(wid, len){
+    var perimeter=wid*2+len*2;
+
+    //return the perimeter
+    return perimeter;
+
+}
