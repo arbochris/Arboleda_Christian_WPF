99fe2a7a9968cbfad452370b4bbccb157bf3dd04
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-14 19:20:38 -0500

    Day 9 File: Anonymous Functions
    Created a new directory for Anonymous Functions, class notes.

---
+++ Day9/js/anonymousFunctions.js
@@ -0,0 +1,10 @@
+/**
+Christian Arboleda
+ Anonymous Functions
+ WPF 1411
+ 11-14-2014
+ */
+
+
+//alert("good to go");
+
--- Day9/js/procedures.js
+++ Day9/js/procedures.js
@@ -24,3 +24,20 @@ function calcArea(width, height){
 
 var returnedArea=calcArea(6,7);
 console.log(returnedArea);
+
+
+//Procedure - console.log the perimeter
+
+function calcPeri(width, height){
+    var peri = width*2+height*2;
+
+    //console.log the peri inside the procedure
+    console.log(peri);
+
+    //Does not return value
+}
+
+
+//function call
+//do not catch with a variable
+calcPeri(6,7);
