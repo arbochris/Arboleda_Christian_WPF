bcfadbfc42707149b72236e5d17f34d94bfbcf7a
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-14 20:38:45 -0500

    Ending Multiple Functions
    Finished class notes.

--- Day9/js/anonymousFunctions.js
+++ Day9/js/anonymousFunctions.js
@@ -8,3 +8,52 @@ Christian Arboleda
 
 //alert("good to go");
 
+
+/*
+basic function
+
+function functionName(){
+    //code goes here
+}
+
+Anonymous Function
+
+var functionName=function(){
+   //code goes here
+
+}
+
+ */
+
+
+//a normal function to find the are of a triangle
+
+function triangleArea(base, height){
+
+    var area=.5*base*height;
+    return area;
+
+
+}
+
+
+//fucntion call - for normal function
+
+var returnedArea=triangleArea(4,5);
+console.log("Returned area for normal function is "+returnedArea);
+
+
+
+//Anonymous Function that calculates triangle area.
+var anonTriArea=function(base, height){
+
+    var area=.5*base*height;
+    return area;
+
+};
+
+
+//Function call for Anonymous Function
+
+var returnedAnonAre=anonTriArea(2,3);
+console.log("The returned area from the anon function is "+returnedAnonAre);
