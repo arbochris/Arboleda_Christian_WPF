eeb28111bdaa6c8949dda045abc84095311a2a5c
arbochris <bocaraca@christians-mbp.local.tld>
2014-11-15 17:25:50 -0500

    Practice folder
    This is a access prompt for establishing when to feed the dogs when one
    is away.

--- Practice/js/Practice.js
+++ Practice/js/Practice.js
@@ -1,5 +1,38 @@
 
-//establish variables
+//Personal Related
+//variables for acces prompt
+
+var answerArray=["Christian Arboleda", "7353"];
+
+var question=prompt("Please enter Name and Last Name:");
+
+while(question===""){
+    question=(prompt("Please enter Name and Last Name:"));
+
+}
+var password=prompt("Now enter password please:");
+
+while(password===""){
+    password=(prompt("Please enter password:"));
+}
+
+//access prompt
+if(question==answerArray[0]){
+}
+
+if(password==answerArray[1]){
+    console.log("Access Granted");
+    alert("Welcome Mr. Arboleda");
+
+}else{
+    console.log("Try again next time");
+    alert("ACCESS DENIED!!");
+    }
+
+
+
+
+//variables for food prompt
 
 var bagFood=480;    //ounces
 var yumiFood=8; //ounces
@@ -10,12 +43,15 @@ var pupsterFood=4;  //ounces
 daysOfFoodLeft=bagFood/(yumiFood+pupsterFood);
 console.log(daysOfFoodLeft);
 
-alert("WARNING!! The "+bagFood+ " ounces of food equals 30 lbs.\n You have "+daysOfFoodLeft+ " days until food runs short.");
+alert("The "+bagFood+ " ounces (30lbs.) of food is under the counter. \n You have "+daysOfFoodLeft+ " days until food runs short.");
+
+
+//
 
 //now i want to know when to feed the dogs
 //established variable
 
-var feedTime=10
+var feedTime=22
 var timeHome=prompt("what time did you get home?\n (24hr input)");
 
 //validated the prompt twice
@@ -27,15 +63,16 @@ if(timeHome==="") {
     timeHome=prompt("(*sigh* Is this thing on?)\n What time did you get home?");
 }
 
-(timeHome<=feedTime)?console.log("Don't feed them yet."):(timeHome>=feedTime)?console.log("Feed them. Pupster gets "+pupsterFood+ " ounces and Yumi gets "+yumiFood+ " ounces.");
-
-/*
 if(timeHome<=feedTime){
 
     console.log("Don't feed them yet.");
 
 }else if(timeHome>=feedTime){
-    console.log("Feed them. Pupster gets "+pupsterFood+ " ounces and Yumi gets "+yumiFood+ " ounces.");
+    console.log("Feed them.");
+    alert("Pupster gets "+pupsterFood+ " ounces (1/2 scoop) and Yumi gets "+yumiFood+ " ounces (1 scoop).");
 }
-**/
+
+
+
+
 
