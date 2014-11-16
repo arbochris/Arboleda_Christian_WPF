9a17085bdc12b0703b024e4dfd9b351356e0ddc7
arbochris <bocaraca@christians-mbp.local.tld>
2014-11-15 23:15:17 -0500

    Update for Assignment 6
    Writing code for an access prompt. Need to input name and password in
    order to continue forward. Then wrote code to validate said code in a
    while loop.

--- Assignment6/conditionals_personal/js/conditionalsPersonal.js
+++ Assignment6/conditionals_personal/js/conditionalsPersonal.js
@@ -17,4 +17,22 @@ Conditionals: Personal
 
 
 
+//First I want to create an access prompt.
+//Originally it's supposed to have different people but for the sake
+//of being fast and accurate I'll just make it for one person.
 
+//First we establish the variables for the prompt:
+
+var answerArray=["Christian Arboleda", "7353"];
+var question=prompt("Please enter Name and Last Name:");
+
+//Looping prompt with validation
+
+while(question===""){
+ question=(prompt("Please enter Name and Last Name:"));
+
+}
+var password=prompt("Now enter password please:");
+
+while(password===""){
+ password=(prompt("Please enter password:"));
