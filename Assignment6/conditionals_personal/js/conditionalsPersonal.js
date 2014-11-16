f8ddbd6ebd4df11ad89e4d0d9ad00c3cee574563
arbochris <bocaraca@christians-mbp.local.tld>
2014-11-16 00:05:14 -0500

    Assignment 6: 2nd prompt and validations
    Established the variables for the prompts. Made the prompts and
    validated them. Also fixed an error on line 62 regarding the variable
    ‘pupsterFood’.

--- Assignment6/conditionals_personal/js/conditionalsPersonal.js
+++ Assignment6/conditionals_personal/js/conditionalsPersonal.js
@@ -59,7 +59,7 @@ if(question==answerArray[0]){   //unless the prompt matches the info input acces
 
 var bagFood=480;  //the value is in ounces.
 var yumiFood=8;  //My dog Yumi's food in ounces.
-var pupster=4;  //My dog Pupster's food in ounces.
+var pupsterFood=4;  //My dog Pupster's food in ounces.
 
 //Now we write the expression to solve the problem:
 
@@ -68,4 +68,27 @@ console.log(daysOfFoodLeft);
 
 alert("The "+bagFood+ " ounces (30lbs.) of food is under the counter. \n You have "+daysOfFoodLeft+ " days until food runs short.");
 
+//Additionally, I want to know WHEN to feed the dogs.
+//Establish variables:
 
+var feedTime=22;  //Value is in 24hr time.
+var timeHome=prompt("What time did you get home? \n (24 hour input)");
+
+//Now we make a prompt asking for the time they got home.
+//Depending on the answer you get a go ahead with a location of the bag or a no feed.
+//we build and validate the prompt:
+
+if(timeHome===""){
+ timeHome = prompt("What time did you get home?\nDo not leave blank!!");  //validation
+}
+if(timeHome===""){
+ timeHome=prompt("(*sigh* Is this thing on?)\n What time did you get home?");  //validation
+}
+
+if(timeHome<=feedTime){
+ console.log("Don't feed them yet.");  //if the time you get home is less than the feeding time you don't feed the dogs.
+
+}else if(timeHome>=feedTime){
+ console.log("Feed them.");
+ alert("Pupster gets "+pupsterFood+ " ounces (1/2 scoop) and Yumi gets "+yumiFood+ " ounces (1 scoop).");  //if the time you get home is more than the feeding time you feed them
+}
