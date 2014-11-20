8b59693720c4357f5a3c5424ceda97e80f22d7ec
arbochris <bocaraca@Christians-MacBook-Pro.local>
2014-11-19 19:22:15 -0500

    Update on WPF Final Exam
    Prompted the user for an item cost and validated the prompt so it’s not
    left blank. Then prompted for a discount percent and validated that
    prompt too.

--- finalExam/js/finalExam.js
+++ finalExam/js/finalExam.js
@@ -11,3 +11,23 @@ Christian Arboleda
 
 //Prompt the user for the cost of an item:
 
+var selectItem=prompt("Please enter the cost of your selected item:");
+
+
+//Now we validate that the prompt is a number and not left blank:
+
+while(selectItem==="" || isNaN(selectItem)){
+
+    selectItem=parseInt(prompt("Please do not leave blank. \n Enter the cost of your selected item:"));
+}
+
+//Now prompt user for a discount number.
+//Number should be between 0 and 100.
+
+var discountPer=prompt("Let's find out your discount price. \n Enter selected item's discount:");
+
+while(discountPer==="" || isNaN(discountPer)){
+
+    discountPer= parseInt(prompt("Please do not leave blank. \n Enter discount of your selected item:"));
+}
+
